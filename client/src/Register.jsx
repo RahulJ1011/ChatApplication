89ef16ffa609e69ffc15c8dd42e7181819512251
rahulj1011 <rahuljr2022@gmail.com>
2024-09-15 23:48:48 +0530

    Initial Commit

---
+++ client/src/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div className='bg-red-950 text-white h-screen'>
+      Register
+    </div>
+  )
+}
+
+export default Register
