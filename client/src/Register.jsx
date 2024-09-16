d242db587682980884a55f048bf925c64e117c81
rahulj1011 <rahuljr2022@gmail.com>
2024-09-16 22:51:12 +0530

    Added register

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register = () => {
+  const [userName,SetUsername] = useState('')
+  const [password,SetPassword] = useState('')
   return (
-    <div className='bg-red-950 text-white h-screen'>
-      Register
+    <div className='bg-gray-600 text-white h-screen flex item-center'>
+      <form className='w-64 mx-auto '>
+        <input 
+        type='text'
+        placeholder='username'
+        className='block w-full rounded-sm p-2 mb-2'
+        />
+        <input 
+        type='password'
+        placeholder='password'
+        className='block w-full rounded-sm p-2 mb-2'
+        />
+        <button
+        className='bg-blue-500 text-white block w-full rounded-sm'
+        >
+          Register
+        </button>
+      </form>
     </div>
   )
 }
