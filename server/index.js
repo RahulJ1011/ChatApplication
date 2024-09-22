8c0afc66a872137c41093e9a589c7369173670c9
rahulj1011 <rahuljr2022@gmail.com>
2024-09-22 13:16:07 +0530

    Added cors

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
 const express = require('express')
+const app = express();
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const user = require('./models/user')
+app.use(cors({
+    origin: 'http://localhost:5173/',
+    credentials:true
+}))
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/Chat');
+app.listen(4000,()=> console.log('server is running'))
